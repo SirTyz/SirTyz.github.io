9203e495a30d3759aa3217726a66b54aba00c3c6
Tyler <tcox2@ar_212_01.harding.edu>
2016-12-14 12:31:26 -0600

    uh oh...

--- assets/js/customScript.js
+++ assets/js/customScript.js
@@ -1,3 +1,8 @@
+<<<<<<< HEAD
+=======
+$(document).ready(function() {
+
+>>>>>>> parent of 535e3bc... minor switcharoo
 //Implements a snackbar popup display.
 function displaySnackbar() {
     // Get the snackbar DIV
@@ -9,3 +14,21 @@ function displaySnackbar() {
     // After 3 seconds, remove the show class from DIV
     setTimeout(function(){ x.className = x.className.replace("show", ""); }, 3000);
 }
+<<<<<<< HEAD
+=======
+
+//Implements a scrolling nav bar.
+  $(window).scroll(function () {
+      //if you hard code, then use console
+      //.log to determine when you want the
+      //nav bar to stick.
+      console.log($(window).scrollTop())
+    if ($(window).scrollTop() > 280) {
+      $('#nav_bar').addClass('navbar-fixed');
+    }
+    if ($(window).scrollTop() < 281) {
+      $('#nav_bar').removeClass('navbar-fixed');
+    }
+  });
+});
+>>>>>>> parent of 535e3bc... minor switcharoo
